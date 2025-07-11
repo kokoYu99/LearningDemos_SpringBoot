f211050763e2f12e59bdc69fe8f467e66b1bdf47
KoKo <you@example.com>
2025-07-11 11:19:18 +0800

    0711-1119

--- springboot_base_hello/src/main/java/com/koko/Main.java
+++ springboot_base_hello/src/main/java/com/koko/Main.java
@@ -2,6 +2,10 @@ package com.koko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 /* 步骤3. 创建启动类 */
 
@@ -12,7 +16,14 @@ public class Main {
         /* 步骤5. 启动程序 */
 
         //1.创建IoC容器; 2.启动tomcat
-        SpringApplication.run(Main.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+
+        //默认加载的组件数量
+        System.out.println("applicationContext.getBeanDefinitionCount() = " + applicationContext.getBeanDefinitionCount());
+
+        //所有加载的组件的名称
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        Arrays.stream(beanDefinitionNames).forEach(System.out::println);
 
     }
 }
---
+++ springboot_base_hello/src/main/java/com/koko/config/Config01.java
@@ -0,0 +1,4 @@
+package com.koko.config;
+
+public class Config01 {
+}
