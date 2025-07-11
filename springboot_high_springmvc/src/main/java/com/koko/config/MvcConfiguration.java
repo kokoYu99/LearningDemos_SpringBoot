afce62ba457659bbc9e994b1447daa0b46abed6d
KoKo <you@example.com>
2025-07-11 23:44:28 +0800

    0711_2344

--- springboot_high_springmvc/src/main/java/com/koko/config/MvcConfiguration.java
+++ springboot_high_springmvc/src/main/java/com/koko/config/MvcConfiguration.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/* 配置类 */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
+    /* 自动装配拦截器 */
     @Autowired
     private MyInterceptor interceptor;
 
+    /* 重写添加拦截器的方法 */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor) //添加拦截器
