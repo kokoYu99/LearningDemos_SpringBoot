b241e68aac2157b6ffca38235fb41c2dc8460035
KoKo <you@example.com>
2025-07-11 23:20:18 +0800

    0711_2319

--- springboot_high_springmvc/src/main/java/com/koko/interceptor/MyInterceptor.java
+++ springboot_high_springmvc/src/main/java/com/koko/interceptor/MyInterceptor.java
@@ -1,4 +1,14 @@
 package com.koko.interceptor;
 
-public class MyInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
 }
