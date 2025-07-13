472f2c5d861d9dc153ba50107fe7abc5f02dca15
KoKo <you@example.com>
2025-07-13 13:16:01 +0800

    0712_1315

---
+++ springboot_high_mybatis/src/main/java/com/koko/springboot_high_mybatis/SpringbootHighMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.koko.springboot_high_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHighMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHighMybatisApplication.class, args);
+    }
+
+}
---
+++ springboot_high_mybatis/src/test/java/com/koko/springboot_high_mybatis/SpringbootHighMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koko.springboot_high_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHighMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
